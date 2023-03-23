dea6b18a3b3e375a0bb7c66b40c503d412080898
Stephen Rudge <sbrudge@comcast.net>
2023-03-22 22:22:12 -0500

    Add project files.

---
+++ Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Gifter.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public int UserProfileId { get; set; }
+        public UserProfile UserProfile { get; set; }
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
---
+++ Repositories/PostRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Gifter.Models;
+using Gifter.Utils;
+
+namespace Gifter.Repositories
+{
+    public class PostRepository : BaseRepository,
+    {
+        public PostRepository(IConfiguration configuration) : base(configuration) { }
+
+        public List<Post> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    Select Id, Title Caption DateCreated, ImageUrl, UserProfileId
+                    From Post
+                    ORDER BY DateCreated";
+                }
+            }
+        }
+        
+    }
+}
